88542e7bbb0fb6983ac05c1dbe0d363f50f6c014
Uladimir Hrydzin <hrydzin@gmail.com>
2024-02-02 02:54:04 +0400

    feat: add avigateToParentDirectory function

--- src/start/start.js
+++ src/start/start.js
@@ -1,5 +1,97 @@
+// import readline from 'readline';
+// import path from 'path';
+// import { redErrorMessage } from '../utils/redMessage.js';
+// // import { navigateToParentDirectory } from '../nav/navigation.js';
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// const [, , fullStringName] = process.argv;
+// const [, username] = fullStringName.split('=');
+
+// export let workingDirectory = process.cwd();
+
+// function printWelcomeMessage() {
+//   console.log(`Welcome to the File Manager, ${username}!`);
+// }
+
+// function printWorkingDirectory() {
+//     console.log(`You are currently in ${workingDirectory}`);
+//   }
+
+// function printGoodbyeMessage() {
+//   console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+// }
+
+// function promptUser() {
+//   rl.question('Please enter your command: ', (input) => {
+//     processUserInput(input);
+//   });
+// }
+
+// function processUserInput(input) {
+//   const command = input.trim().toLowerCase();
+
+//   switch (command) {
+//     case '.exit':
+//       printGoodbyeMessage();
+//       rl.close();
+//       break;
+//     case '':
+//       printErrorMessage('Empty input.  Please enter a command.');
+//       promptUser();
+//       break;
+// // up
+//       case 'up':
+//       case 'cd ..':
+//       navigateToParentDirectory();
+//       // printWorkingDirectory();
+//       promptUser();
+//       break;
+
+//     default:
+//       printErrorMessage(`Unknown operation "${command}"`);
+//       promptUser();
+//       break;
+//   }
+// }
+
+// function navigateToParentDirectory() {
+//   const parentDirectory = path.resolve(workingDirectory, '..');
+
+//   if (path.relative(parentDirectory, path.sep) === '') {
+//     printErrorMessage('Cannot go above the root directory.');
+//   } else {
+//     workingDirectory = parentDirectory;
+//     printWorkingDirectory();
+//   }
+// }
+
+// function printErrorMessage(message) {
+//   redErrorMessage(`Error: ${message}`, '31');
+// }
+
+// printWelcomeMessage();
+// printWorkingDirectory();
+// promptUser();
+
+// rl.on('close', () => {
+//   printGoodbyeMessage();
+//   rl.close();
+// });
+
+// rl.on('error', (err) => {
+//   redErrorMessage(`An unexpected error occurred: ${err}`, '31');
+//   rl.close();
+// });
+
+
+// start.js
+
 import readline from 'readline';
-import { redErrorMessage } from '../utils/redMessage.js';
+import { processUserInput, printWelcomeMessage, printWorkingDirectory, printGoodbyeMessage, printErrorMessage } from '../comands/comands.js';
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -9,66 +101,24 @@ const rl = readline.createInterface({
 const [, , fullStringName] = process.argv;
 const [, username] = fullStringName.split('=');
 
-const workingDirectory = process.cwd();
+let workingDirectory = process.cwd();
 
-function printWelcomeMessage() {
-  console.log(`Welcome to the File Manager, ${username}!`);
-}
-
-function printWorkingDirectory() {
-    console.log(`You are currently in ${workingDirectory}`);
-  }
-
-function printGoodbyeMessage() {
-  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
-}
-function promptUser() {
-  rl.question('Enter your command: ', (input) => {
-    processUserInput(input);
-  });
-}
-
-// function processUserInput(input) {
-//   if (input.trim().toLowerCase() === '.exit') {
-//     rl.close();
-//   } else {
-//     console.log(`You entered: ${input}`);
-//     promptUser();
-//   }
-// }
-
-function processUserInput(input) {
-  const command = input.trim().toLowerCase();
-
-  switch (command) {
-    case '.exit':
-      rl.close();
-      break;
-    case '':
-      printErrorMessage('Invalid empty input.  Please enter a command.');
-      promptUser();
-      break;
-    default:
-      printErrorMessage(`unknown operation "${command}"`);
-      promptUser();
-      break;
-  }
-}
-
-function printErrorMessage(message) {
-  redErrorMessage(`Error: ${message}`, '31');
-}
-
-printWelcomeMessage();
-printWorkingDirectory();
+printWelcomeMessage(username);
+printWorkingDirectory(workingDirectory);
 promptUser();
 
 rl.on('close', () => {
-  printGoodbyeMessage();
+  printGoodbyeMessage(username);
   rl.close();
 });
 
 rl.on('error', (err) => {
-  redErrorMessage(`An unexpected error occurred: ${err}`, '31');
+  printErrorMessage(`An unexpected error occurred: ${err}`);
   rl.close();
 });
+
+export function promptUser() {
+  rl.question('Please enter your command: ', (input) => {
+    workingDirectory = processUserInput(input.trim().toLowerCase(), workingDirectory);
+  });
+}
