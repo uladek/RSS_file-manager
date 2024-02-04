dda994596097b1aaf5f940898ccfbd534865b5c9
Uladimir Hrydzin <hrydzin@gmail.com>
2024-02-04 22:48:44 +0400

    fix: rn command

--- src/commands/rename.js
+++ src/commands/rename.js
@@ -1,11 +1,21 @@
-import { rename } from 'node:fs/promises';
+import { access, rename } from 'node:fs/promises';
+import path from 'node:path';
 import { printErrorMessage } from '../commands/commands.js';
 
 export const renameFile = async (filePath, newFilename) => {
   try {
-    await rename(filePath, newFilename);
-    console.log(`File renamed successfully to ${newFilename}`);
-  } catch (error) {
-    printErrorMessage(`Operation Failed: File "${filePath}" not found.`);
+    const directory = path.dirname(filePath);
+    const newFilePath = path.join(directory, newFilename);
+
+    await access(newFilePath);
+
+    printErrorMessage(`Operation Failed: File "${newFilename}" already exists.`);
+  } catch (accessError) {
+    try {
+      await rename(filePath, path.join(path.dirname(filePath), newFilename));
+      console.log(`File renamed successfully to ${newFilename}`);
+    } catch (renameError) {
+      printErrorMessage(`Operation Failed: File "${filePath}" not found.`);
+    }
   }
 };
